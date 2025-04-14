5026994b8ca06b05ff856b00136c7904ec1fadbe
succi0303 <succi0303@gmail.com>
2025-04-14 21:40:57 +0900

    To fix package load error, put package config into init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,25 +1,36 @@
-;;; init.el --- Entry point -*- lexical-binding: t -*-
+;;; init.el --- Entry point -*- coding: utf-8 ; lexical-binding: t -*-
 ;;; Commentary:
 ;; Emacs init entry point. Loads modules from lisp/.
 
 ;;; Code:
 
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file :no-error-if-file-is-missing)
+
 ;; Set UTF-10 and suppress startup screen
 (set-language-environment "UTF-8")
 (setq inhibit-startup-screen t)
 
-;; Set up custom file
-(setq custom-file (locate-user-emacs-file "custom.el"))
-(when (file-exists-p custom-file)
-  (load custom-file))
+;; Package manager configuration
+(require 'package)
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+	("gnu" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
 
-;; Add lisp/ to load-path
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; Add lisp/ to load-path & modules
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
-;; Load modules
 (require 'init-core)
 (require 'init-ui)
-(require 'init-package)
+;(require 'init-package)
 (require 'init-completion)
 (require 'init-org)
 (require 'init-git)
