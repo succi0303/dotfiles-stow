627c1c63e357aca25b5425ddc228c30a92511e6d
Takashi Sugimoto <succi0303@gmail.com>
2025-04-16 14:09:38 +0900

    terraform & shell script configuration

--- emacs/.emacs.d/lisp/init-prog.el
+++ emacs/.emacs.d/lisp/init-prog.el
@@ -3,11 +3,29 @@
 ;; Language support, LSP (eglot), and code editing enhancements.
 ;;; Code:
 
+(use-package eglot
+  :defer t
+  :config
+  (add-to-list 'eglot-server-programs
+	       '(sh-mode . ("bash-language-server" "start"))
+	       '(terraform-mode . ("terraform-ls" "serve"))))
 (use-package flymake
   :defer t)
 
+;; shell script
+(use-package sh-script
+  :mode (("\\.\\(sh\\|bash\\|zsh\\)\\'" . sh-mode))
+  :hook ((sh-mode . eglot-ensure))
+  :custom
+  (sh-basic-offset 2)
+  (sh-indentation 2))
+
+;; terraform
 (use-package terraform-mode
-  :custom (terraform-indent-level 4))
+  :mode ("\\.tf\\'" . terraform-mode)
+  :hook ((terraform-mode . terraform-format-on-save-mode))
+  :config
+  (setq terraform-format-on-save-mode t))
 
 (provide 'init-prog)
 ;;; init-prog.el ends here
