e3bdfa025c97c3ca8cfdf9d88a556b0b7d4858a5
Takashi Sugimoto <succi0303@gmail.com>
2025-04-19 15:36:16 +0900

    Invoke mise install

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ sudo add-apt-repository -y ppa:ubuntuhandbook1/emacs
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y \
      gpg \
+     gpg-agent \
      sudo \
      curl \
      wget \
@@ -22,11 +23,15 @@ DOTDIR="$( cd "$( dirname "$0" )" && pwd )"
 stow -d $DOTDIR -t $HOME tmux
 stow -d $DOTDIR -t $HOME emacs
 stow -d $DOTDIR -t $HOME vim
+stow -d $DOTDIR -t $HOME mise
 
 # mise
 curl https://mise.run | sh
 echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
+mise install
 
 # vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+npm install -g bash-language-server
