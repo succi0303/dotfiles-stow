920c793a8684defdb3ebf0638375557893e40d72
Takashi Sugimoto <succi0303@gmail.com>
2025-04-26 11:06:14 +0900

    Add ddskk for emacs

--- emacs/.emacs.d/lisp/init-doc.el
+++ emacs/.emacs.d/lisp/init-doc.el
@@ -9,5 +9,21 @@
   :init
   (setq markdown-command "pandoc"))
 
+;; skk
+(use-package ddskk
+  :ensure t
+  :bind (("C-x j" . skk-mode))
+  :init
+  (setq default-input-method "japanese-skk")
+  (setq skk-start-henkan-with-inline 'ask)
+  (setq skk-show-inline t)
+  (setq skk-use-jisx0201-input-method t)
+  (setq skk-use-look t)
+  (setq skk-auto-okuri-process t)
+  (setq skk-save-jisyo-instantly t)
+  (setq skk-isearch-start-mode 'latin))
+
 (provide 'init-doc)
 ;;; init-prog.el ends here
+
+
