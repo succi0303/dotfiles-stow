4eb35e1e0aaf51c79a5be77d9646cf00603b8ad3
Takashi Sugimoto <succi0303@gmail.com>
2025-04-26 15:47:07 +0900

    install & configure ddskk

--- emacs/.emacs.d/lisp/init-doc.el
+++ emacs/.emacs.d/lisp/init-doc.el
@@ -13,17 +13,23 @@
 (use-package ddskk
   :ensure t
   :bind (("C-x j" . skk-mode))
+  :hook ((skk-mode . (lambda()
+		       (require 'context-skk)))
+	 (find-file . (lambda()
+			 (skk-latin-mode t))))
   :init
+  (setq skk-user-directory "~/.ddskk")
   (setq default-input-method "japanese-skk")
   (setq skk-start-henkan-with-inline 'ask)
-  (setq skk-show-inline t)
+  (setq skk-sticky-key ";")
+  (setq skk-use-color-cursor t)
   (setq skk-use-jisx0201-input-method t)
   (setq skk-use-look t)
+  (setq skk-dcomp-activate t)
+  (setq skk-dcomp-multiple-activate t)
   (setq skk-auto-okuri-process t)
   (setq skk-save-jisyo-instantly t)
   (setq skk-isearch-start-mode 'latin))
 
 (provide 'init-doc)
 ;;; init-prog.el ends here
-
-
